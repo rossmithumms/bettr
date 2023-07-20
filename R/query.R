9e534bc61ad8c26dcdf456fccc288146a8f80a52
rossmith@med.umich.edu <rossmith@med.umich.edu>
2023-07-20 22:39:01 +0000

    Altering call to janitor

--- R/query.R
+++ R/query.R
@@ -123,8 +123,7 @@ get_rows <- function(binds, connection_name, sql, suppress_bind_logging = FALSE)
 
       output <- output %>%
           data.table::rbindlist() %>%
-          tibble::tibble() # %>%
-          # janitor::clean_names()
+          tibble::as_tibble(.name_repair = janitor::make_clean_names)
 
       message(".... THIS should actually result in a 16,500-ish number:")
       message(dplyr::count(output))
